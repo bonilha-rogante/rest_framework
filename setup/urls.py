f499a4d2da8f66171150e11a1eb0949ef434ecd4
Guilherme <bonilha.rogante@gmail.com>
2024-08-12 22:24:30 -0300

    Curso Rest Framework Alura

---
+++ setup/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculasCurso
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudante', EstudanteViewSet, basename='Estudantes')
+router.register('curso', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas/', ListaMatriculaEstudante.as_view()),
+    path('cursos/<int:pk>/matriculas/', ListaMatriculasCurso.as_view()),
+]
